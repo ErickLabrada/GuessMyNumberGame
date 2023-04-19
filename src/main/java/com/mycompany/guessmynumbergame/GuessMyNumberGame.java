13b074abd6c8ce5ff9a61282acb43332f7eb6b08
Erick Labrada <Erick.labrada233380@potros.itson.edu.mx>
2023-04-19 01:26:14 -0700

    <Inicio> Creación del proyecto

---
+++ src/main/java/com/mycompany/guessmynumbergame/GuessMyNumberGame.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guessmynumbergame;
+
+/**
+ *
+ * @author Erick
+ */
+public class GuessMyNumberGame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
